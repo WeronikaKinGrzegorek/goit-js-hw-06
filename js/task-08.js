c93676508fe59585c81dd4b60f7b2f9961373bb2
WeronikaKinGrzegorek <126391598+WeronikaKinGrzegorek@users.noreply.github.com>
2023-07-23 22:07:33 +0200

    finished task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
+const submitButton = document.querySelector("button");
+loginForm.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("All fields must be completed");
+  } else {
+    const finalObject = {
+      email: email.value,
+      password: password.value,
+    };
 
+    console.log(finalObject);
+
+    event.currentTarget.reset();
+  }
+}
